2236e01bf38b957c3fda37b36399df971019be65
P.M.S.V. Jinadasa <sanodyaj@gmail.com>
2026-04-03 08:48:35 +0530

    route update

--- Mentra/routes/web.php
+++ Mentra/routes/web.php
@@ -45,6 +45,7 @@ Route::get('/articals', [MusicController::class, 'articalindex'])->name('artical
 Route::get('/community', [QuestionController::class, 'index'])->name('community');
 Route::get('/top-focuzers', [ChallengeController::class, 'topFocuzers'])->name('topfocuzers.index');
 Route::get('/study-challenges', [ChallengeController::class, 'showChallenges'])->name('userchallenges.index');
+Route::post('/articles', [StudyArticleController::class, 'storeuserartical'])->name('articles.store');
 
 
 Route::middleware(['auth'])->group(function () {
@@ -63,6 +64,8 @@ Route::middleware(['auth'])->group(function () {
     Route::post('/questions', [QuestionController::class, 'store'])->name('questions.store');
     Route::get('/questions/{question}', [QuestionController::class, 'show'])->name('questions.show');
     Route::post('/questions/{question}/answers', [AnswerController::class, 'store'])->name('answers.store');
+    Route::put('/questions/{id}', [QuestionController::class, 'update'])->name('questions.update');
+
 
     // admin
     Route::get('/dashboard', function () {
@@ -89,6 +92,8 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/admin/articles/{id}', [StudyArticleController::class, 'update'])->name('Article.update');
     Route::delete('/admin/articles/{id}', [StudyArticleController::class, 'destroy'])->name('Article.destroy');
     Route::post('/admin/articles/{id}/restore', [StudyArticleController::class, 'restore'])->name('Article.restore');
+    Route::post('/articles/{id}/approve', [StudyArticleController::class, 'approve'])->name('Article.approve');
+    Route::post('/articles/{id}/unapprove', [StudyArticleController::class, 'unapprove'])->name('Article.unapprove');
 
 
 });
